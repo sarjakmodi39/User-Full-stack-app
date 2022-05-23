327f99a2d4e171b21c238177d43da688d432fe8f
sarjakmodi39 <60146762+sarjakmodi39@users.noreply.github.com>
2022-05-23 12:01:17 +0530

    Update userdetails.js

--- src/components/userdetails.js
+++ src/components/userdetails.js
@@ -8,7 +8,6 @@ const UserDetails = () => {
   const [isEdit, setIsEdit] = useState(false);
   const [currentUser, setCurrentUser] = useState();
   const [name, setName] = useState("");
-  const [id, setId] = useState();
   const [isModalVisible, setIsModalVisible] = useState(false);
   useEffect(() => {
     getUserDetails();
@@ -23,32 +22,6 @@ const UserDetails = () => {
       });
   };
 
-  const changeName = (user) => {
-    setIsEdit(true);
-    setCurrentUser(user);
-    setName(user.name);
-  };
-
-  const handleNameChange = (event) => {
-    setName(event.target.value);
-    console.log(name);
-  };
-
-  const handleChangeName = () => {
-    fetch("http://localhost:3000/users/updateUser", {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: name, id: currentUser.id }),
-    })
-      .then((result) => result.json())
-      .then((response) => {
-        console.log(response);
-        getUserDetails();
-        alert(response.message);
-        setIsEdit(false);
-      });
-  };
-
   const handleOpenModal = () => {
     setIsModalVisible(true);
   };
@@ -57,61 +30,23 @@ const UserDetails = () => {
     setIsModalVisible(false);
   };
 
-  const handleDeleteUser = (user) => {
-    console.log("hee");
-    fetch("http://localhost:3000/users/deleteUser", {
-      method: "DELETE",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ id: user.id }),
-    })
-      .then((result) => result.json())
-      .then((response) => {
-        getUserDetails();
-        alert(response.message);
-      });
-  };
-
   return (
     <>
       <div className="container">
         {users.map((user) => {
           return (
             <>
-              <div key={user.id} className="card">
+              <div key={user.id}>
                 <UserCard
-                  name={user.name}
-                  id={user.id}
-                  mobileNumber={user.mobileNumber}
+                  user={user}
+                  getUserDetails={getUserDetails}
                 ></UserCard>
-                <button
-                  onClick={() => {
-                    changeName(user);
-                  }}
-                >
-                  Edit
-                </button>
-                <button
-                  className="button"
-                  type="button"
-                  onClick={() => {
-                    handleDeleteUser(user);
-                  }}
-                >
-                  Delete
-                </button>
               </div>
             </>
           );
         })}
       </div>
-      {isEdit && (
-        <div>
-          <input type="text" onChange={handleNameChange} value={name} />
-          <button type="submit" onClick={handleChangeName}>
-            submit
-          </button>
-        </div>
-      )}
+
       <button type="submit" onClick={handleOpenModal}>
         Add User
       </button>
