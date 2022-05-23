42f5625bc28c487dc873525e817f72c3c15b24fe
sarjakmodi39 <60146762+sarjakmodi39@users.noreply.github.com>
2022-05-23 12:00:39 +0530

    Update usercard.js

--- src/components/usercard.js
+++ src/components/usercard.js
@@ -1,10 +1,76 @@
-const UserCard = ({ id, name, mobileNumber }) => {
+import { useState } from "react";
+const UserCard = ({ getUserDetails, user }) => {
+  const [isEdit, setIsEdit] = useState(false);
+  const [currentUser, setCurrentUser] = useState();
+  const [name, setName] = useState("");
+
+  const changeName = (user) => {
+    setIsEdit(true);
+    setCurrentUser(user);
+    setName(user.name);
+  };
+  const handleNameChange = (event) => {
+    setName(event.target.value);
+    console.log(name);
+  };
+
+  const handleChangeName = () => {
+    fetch("http://localhost:3000/users/updateUser", {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ name: name, id: currentUser.id }),
+    })
+      .then((result) => result.json())
+      .then((response) => {
+        console.log(response);
+        getUserDetails();
+        alert(response.message);
+        setIsEdit(false);
+      });
+  };
+  const handleDeleteUser = (user) => {
+    console.log("hee");
+    fetch("http://localhost:3000/users/deleteUser", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: user.id }),
+    })
+      .then((result) => result.json())
+      .then((response) => {
+        getUserDetails();
+        alert(response.message);
+      });
+  };
   return (
     <>
-      <div key={id} className="card">
-        <p>Id={id}</p>
-        <p>Name={name}</p>
-        <p>Mobile={mobileNumber}</p>
+      <div key={user.id} className="card">
+        <p>Id={user.id}</p>
+        <p>Name={user.name}</p>
+        <p>Mobile={user.mobileNumber}</p>
+        {isEdit && (
+          <div>
+            <input type="text" onChange={handleNameChange} value={name} />
+            <button type="submit" onClick={handleChangeName}>
+              submit
+            </button>
+          </div>
+        )}
+        <button
+          onClick={() => {
+            changeName(user);
+          }}
+        >
+          Edit
+        </button>
+        <button
+          className="button"
+          type="button"
+          onClick={() => {
+            handleDeleteUser(user);
+          }}
+        >
+          Delete
+        </button>
       </div>
     </>
   );
